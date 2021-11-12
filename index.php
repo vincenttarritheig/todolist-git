e0ae2e227f44fd8b3343ec456b95c95f53ecc04a
vincenttarritheig <vincent.tarrit@heig-vd.ch>
2021-11-12 15:38:56 +0100

    define with env var

--- index.php
+++ index.php
@@ -8,11 +8,11 @@ error_reporting(1);
 define('BASE_URL', '/');
 
 // Database connection parameters.
-define('DB_USER', 'todolist');
-define('DB_PASS', 'todolist');
-define('DB_NAME', 'todolist');
-define('DB_HOST', '127.0.0.1');
-define('DB_PORT', '3306');
+define('DB_USER', getenv('TODOLIST_DB_USER') ?: 'todolist');
+define('DB_PASS', getenv('TODOLIST_DB_PASS'));
+define('DB_NAME', getenv('TODOLIST_DB_NAME') ?: 'todolist');
+define('DB_HOST', getenv('TODOLIST_DB_HOST') ?: '127.0.0.1');
+define('DB_PORT', getenv('TODOLIST_DB_PORT') ?: '3306');
 
 $db = new PDO('mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
 $items = array();
